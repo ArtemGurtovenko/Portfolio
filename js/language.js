4de5f16fed043e043d235d1107d4ef6e214c7c24
ArtemGurtovenko <74982630+ArtemGurtovenko@users.noreply.github.com>
2024-02-22 20:17:44 +0200

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,14 @@
+const langArr = {
+    "name": {
+      "en": "Artem Hurtovenko",
+      "ua": "Артем Гуртовенко",
+    },
+    "title": {
+      "en": "I am Frontend Developer from Ukraine",
+      "ua": "Я веб розробник з України",
+    },
+    "text": {
+      "en": "Hello, I'm a web developer from Ukraine. I have been doing what I love since 2020. I have good knowledge of html / css / js.Let's do great things together. I'll be happy to help you with this",
+      "ua": "Привіт, я веб-розробник з України. З 2020 року займаюся улюбленою справою. Добре знаю html / css / js.Давайте робити великі справи разом.Я буду радий вам у цьому допомогти",
+    }
+  }
